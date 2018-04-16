235b4cd573515594b33e6d295ed2ca909ef45594
Sam Johnson <sam@durosoft.com>
2018-04-16 03:30:11 -0400

    test compiling a payload

--- functions/compile-crystal/compile.js
+++ functions/compile-crystal/compile.js
@@ -2,10 +2,11 @@ const child_process = require('child_process');
 const process = require('process');
 const tmp = require('tmp');
 const fs = require('fs-extra');
+const unzipper = require('unzipper');
 
 const tagPrefix = '<html><body>You are being <a href="https://github.com/crystal-lang/crystal/releases/tag/';
 const tagSuffix = '">redirected</a>.</body></html>';
-const downloadCmd = 'curl -L --silent https://github.com/crystal-lang/crystal/releases/latest | grep "linux-x86_64.tar.gz\\\" rel"';
+const downloadCmd = 'curl -L --silent https://github.com/crystal-lang/crystal/releases | grep "linux-x86_64.tar.gz" | grep VERSION | grep href'
 
 function silentCmd(command) {
   return child_process.execSync(command).toString();
@@ -27,17 +28,16 @@ function latestCrystalVersion() {
 
 function getCrystalDownloadLink(version) {
   if(version == null) version = latestCrystalVersion();
-  var targetCmd = downloadCmd.replace('latest', version);
+  var targetCmd = downloadCmd.replace('VERSION', version);
   var result = silentCmd(targetCmd).trim().replace('<a href="', '').replace('" rel="nofollow">', '');
   return 'https://github.com'+result;
 }
 
-function compile() {
+function compile(projectId, payload, version) {
   console.log('preparing to compile...');
   console.log();
   var dir = tmp.dirSync();
   process.chdir(dir.name);
-  var version = null;
   if(!version) {
     console.log('crystal version not specified, finding latest version...');
     version = latestCrystalVersion();
@@ -54,10 +54,44 @@ function compile() {
   console.log();
   console.log('testing that crystal binary is installed and runnable...');
   var crystalBinary = dir.name + '/crystal-'+version+'/bin/crystal';
+  var shardsBinary = dir.name + '/crystal-'+version+'/bin/shards';
   var test = cmd(crystalBinary + ' --version');
   if(!test.includes('Crystal ' + version)) throw 'crystal binary was not installed correctly';
+  process.env.PATH += ':' + dir.name;
   console.log();
-  fs.removeSync(dir.name);
+  console.log('extracting payload...');
+  var dir2 = tmp.dirSync();
+  process.chdir(dir2.name);
+  fs.writeFileSync('./payload.zip', payload);
+  fs.createReadStream('./payload.zip').pipe(unzipper.Extract({ path: '.' })).on('finish', function() {
+    fs.removeSync('./payload.zip');
+    var executable_name = fs.readdirSync('.')[0];
+    process.chdir(executable_name);
+    console.log('finished extracting payload');
+    cmd('ls -la');
+    console.log('installing shards...');
+    cmd(shardsBinary + ' install --release');
+    console.log();
+    console.log('compiling with crystal ' + version + '...');
+    cmd(crystalBinary + ' build ./src/*.cr -o ' + executable_name + ' --release');
+    console.log();
+    cmd('./'+executable_name);
+    setTimeout(function() {
+      try { fs.removeSync(dir2.name); } catch(e) {}
+      try { fs.removeSync(dir.name); } catch(e) {}
+    }, 10);
+  });
 }
 
-compile();
+exports.init = function(req, res) {
+  var projectId = req.body.projectId;
+  var payload = req.body.payload;
+  var version = req.body.version;
+  compile(projectId, payload, version);
+}
+
+function test() {
+  compile('blockvue-spaces', fs.readFileSync('./test.zip'));
+}
+
+test();
