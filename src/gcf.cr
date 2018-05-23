0c8baba3919f3fa076ef44c58d35f6a939d4e0d4
Sam Johnson <sam@durosoft.com>
2018-05-22 21:32:18 -0400

    basic command line options parsing

--- src/gcf.cr
+++ src/gcf.cr
@@ -1,6 +1,24 @@
-require "./gcf/*"
+APPNAME = "gcf.cf"
+APPBIN = "gcf"
 
-# TODO: Write documentation for `Gcf`
-module Gcf
-  # TODO: Put your code here
+require "./gcf/*"
+require "option_parser"
+
+def print_version
+  puts ""
+  puts "#{APPNAME} v#{GCF::VERSION}"
+  puts ""
+end
+
+project_id = ""
+source_path = "."
+function_name = ""
+
+OptionParser.parse! do |parser|
+  parser.banner = "usage: #{APPBIN} [arguments]"
+  parser.on("-h", "--help", "show this help") { puts ""; puts parser; puts "" }
+  parser.on("-p", "--project", "sets the Google project ID for cloud functions, defaults to current gcloud project id") { |v| project_id = v }
+  parser.on("-s", "--source", "path or git link to source code to be deployed, defaults to '.'") { |v| source_path = v }
+  parser.on("-n", "--name", "cloud function name, defaults to name of directory or repo") { |v| function_name = v }
+  parser.on("-v", "--version", "prints the version") { print_version }
 end
--- src/gcf/version.cr
+++ src/gcf/version.cr
@@ -1,3 +1,3 @@
-module Gcf
+module GCF
   VERSION = "0.1.0"
 end
