b01d663b701524acb649b34e572175b15b03b3a8
Sam Johnson <sam@durosoft.com>
2018-05-24 20:43:39 -0400

    add memory config option

--- src/gcf.cr
+++ src/gcf.cr
@@ -1,5 +1,6 @@
 APPNAME = "gcf.cr"
 APPBIN = "gcf"
+POSSIBLE_MEMORY_CONFIGS = ["128 MB", "256 MB", "512 MB", "1 GB", "2 GB"]
 
 require "./gcf/*"
 require "option_parser"
@@ -17,6 +18,8 @@ source_path = "."
 function_name = ""
 http_trigger = ""
 region = "us-central1"
+function_memory = "128 MB"
+
 run_deploy = false
 
 options_parser = nil
@@ -30,6 +33,7 @@ OptionParser.parse! do |parser|
   parser.on("-s PATH", "--source PATH", "path or git link to source code to be deployed, defaults to '.'") { |v| source_path = v }
   parser.on("-n NAME", "--name NAME", "cloud function name, defaults to name of directory or repo") { |v| function_name = v }
   parser.on("-r REGION", "--region REGION", "region for cloud function deployment, only us-central1 is valid") { |v| region = v }
+  parser.on("-m MEMORY", "--memory MEMORY", "ram for cloud function, valid: 128 MB | 256 MB | 512 MB | 1 GB | 2 GB") { |v| function_memory = v }
   parser.on("-v", "--version", "prints the version") { print_version }
   options_parser = parser
 end
@@ -51,7 +55,7 @@ unless run_deploy
 end
 
 print_version
-puts "starting deployment..."
+puts "preparing for deployment..."
 
 # check for valid region
 if region != "us-central1"
@@ -76,3 +80,9 @@ puts " => function_name set to \"#{function_name}\""
 # massage http_trigger
 http_trigger = "https://#{region}-#{project_id}.cloudfunctions.net/#{function_name}"
 puts " => http_trigger set to \"#{http_trigger}\""
+
+# massage memory
+unless POSSIBLE_MEMORY_CONFIGS.includes?(function_memory)
+  raise "#{function_memory} is not a valid memory configuration. Must be one of #{POSSIBLE_MEMORY_CONFIGS}"
+end
+puts " => function_memory set to #{function_memory}"
