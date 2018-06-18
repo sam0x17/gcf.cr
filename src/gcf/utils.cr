e23e1de8c26135197ad687390d7b83bcc6b8592f
Sam Johnson <sam@durosoft.com>
2018-06-18 06:21:44 -0400

    begin modularization

--- src/gcf/utils.cr
+++ src/gcf/utils.cr
@@ -1,50 +1,52 @@
-def polite_raise!(message)
-  puts ""
-  puts "error: #{message}"
-  puts ""
-  exit 1
-end
+module GCF
+  def self.polite_raise!(message)
+    puts ""
+    puts "error: #{message}"
+    puts ""
+    exit 1
+  end
 
-def app_installed?(bin)
-  `which #{bin}` != ""
-end
+  def self.app_installed?(bin)
+    `which #{bin}` != ""
+  end
 
-def require_app!(bin)
-  return if app_installed? bin
-  polite_raise! "#{bin} must be installed to use #{APPNAME}"
-end
+  def self.require_app!(bin)
+    return if app_installed? bin
+    polite_raise! "#{bin} must be installed to use #{APPNAME}"
+  end
 
-def docker_available?
-  !`docker info`.includes? "denied"
-end
+  def self.docker_available?
+    !`docker info`.includes? "denied"
+  end
 
-def gcloud_project_id
-  project_id = `gcloud config get-value project`.strip
-  puts " => obtained project ID \"#{project_id}\" from gcloud"
-  project_id
-end
+  def self.gcloud_project_id
+    project_id = `gcloud config get-value project`.strip
+    puts " => obtained project ID \"#{project_id}\" from gcloud"
+    project_id
+  end
 
-def random_alpha_numeric
-  "abcdefghijklmnopqrstuvwxyz0123456789".chars.sample
-end
+  def self.random_alpha_numeric
+    "abcdefghijklmnopqrstuvwxyz0123456789".chars.sample
+  end
 
-def random_string(length)
-  st = ""
-  length.times { st += random_alpha_numeric }
-  st
-end
+  def self.random_string(length)
+    st = ""
+    length.times { st += random_alpha_numeric }
+    st
+  end
 
-def temp_dir(prefix, create = true)
-  dir = "/tmp/#{prefix}-#{Time.now.epoch}"
-  FileUtils.mkdir_p dir
-  FileUtils.rm_rf dir # delete if existed before
-  FileUtils.mkdir_p(dir) if create
-  at_exit { FileUtils.rm_rf dir }
-  dir
-end
+  def self.temp_dir(prefix, create = true)
+    dir = "/tmp/#{prefix}-#{Time.now.epoch}"
+    FileUtils.mkdir_p dir
+    FileUtils.rm_rf dir # delete if existed before
+    FileUtils.mkdir_p(dir) if create
+    at_exit { FileUtils.rm_rf dir }
+    dir
+  end
 
-def valid_version?(version)
-  !!(/^v?[0-9]+.[0-9]+.[0-9]+$/ =~ version)
+  def self.valid_version?(version)
+    !!(/^v?[0-9]+.[0-9]+.[0-9]+$/ =~ version)
+  end
 end
 
 module FileUtils
